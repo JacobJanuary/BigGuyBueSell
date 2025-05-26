0989cec4917622610a062cd8c97b2f10592248f2
JacobJanuary <jacob.smartfox@gmail.com>
2025-05-26 18:10:57 +0300

    bybit+binance - good job

--- exchanges/coinbase/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Модуль для работы с Coinbase Advanced Trade API."""
-from .client import CoinbaseClient
-from .analyzer import CoinbaseAnalyzer
-
-__all__ = ['CoinbaseClient', 'CoinbaseAnalyzer']
